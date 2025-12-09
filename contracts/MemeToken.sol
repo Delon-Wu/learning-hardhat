3ec5f6f121292ae0b833cf7bd8dce9e8a28250e9
JinLong Wu <2474323762@qq.com>
2025-12-09 17:53:46 +0800

    update

--- contracts/MemeToken.sol
+++ contracts/MemeToken.sol
@@ -15,18 +15,69 @@ contract MemeToken is
     UUPSUpgradeable,
     ReentrancyGuardUpgradeable
 {
-    mapping(address => uint256) private _balances;
+    address public marketWallet;
+    address public developmentWallet;
+    address public liquidityWallet;
+
+    uint256 public buyMarketTax;
+    uint256 public buyDevelopmentTax;
+    uint256 public buyLiquidityTax;
+
+    uint256 public sellMarketTax;
+    uint256 public sellDevelopmentTax;
+    uint256 public sellLiquidityTax;
+
+    uint256 public constant MAX_TAXT_RAGE = 1500;
+
+    mapping(address => bool) public isExcludedFromFee;
+
+    mapping(address => bool) public automatedMarketMakerPairs; // DEX 交易对地址
+
+    bool private inSwap;
+    modifier lockTheSwap {
+        inSwap = true;
+        _;
+        inSwap = false;
+    }
 
     constructor() {
         _disableInitializers();
     }
 
-    function initialize() public initializer {
-        __ERC20_init("MemeToken", "MMTK");
-        __Ownable_init(msg.sender);
+    function initialize(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        address _marketWallet,
+        address _developmentWallet,
+        address _liquidityWallet
+    ) public initializer {
+        __ERC20_init(name, symbol);
+        __Ownable_init();
         __ReentrancyGuard_init();
 
-        
+        require(_marketWallet != address(0), "Market Wallet cannot be zero");
+        require(_developmentWallet != address(0), "Development Wallet cannot be zero");
+        require(_liquidityWallet != address(0), "Liquidity Wallet cannot be zero");
+        marketWallet = _marketWallet;
+        developmentWallet = _developmentWallet;
+        liquidityWallet = _liquidityWallet;
+
+        buyMarketTax = 200;
+        buyDevelopmentTax = 100;
+        buyLiquidityTax = 200;
+
+        sellMarketTax = 400;
+        sellDevelopmentTax = 200;
+        sellLiquidityTax = 400;
+
+        isExcludedFromFee[msg.sender] = true;
+        isExcludedFromFee[address(this)] = true; // ???
+        isExcludedFromFee[marketWallet] = true;
+        isExcludedFromFee[developmentWallet] = true;
+        isExcludedFromFee[liquidityWallet] = true;
+
+        _mint(msg.sender, initialSupply);
     }
     
     function _authorizeUpgrade(
@@ -38,11 +89,36 @@ contract MemeToken is
     // │  ├─ 税费计算逻辑
     // │  └─ 白名单管理
 
-    function _transfer(address from, address to, uint256 amount) public override(ERC20Upgradeable) returns(bool) {
-        uint256 
-        transfer();
-        return true;
+    function _transfer(address from, address to, uint256 amount) internal nonReentrant override(ERC20Upgradeable) {
+        if (from == address(0) || to == address(0)) {
+            super._transfer(from, to, amount);
+            return;
+        }
+        // 在交换中，避免递归收税
+        if (inSwap) {
+            super._transfer(from, to, amount);
+            return;
+        }
+
+        bool takeFee = true;
+        if (isExcludedFromFee[from] || isExcludedFromFee[to]) {
+            takeFee = false;// 白名单
+        }
+        if (!automatedMarketMakerPairs[from] && !automatedMarketMakerPairs[to]) {
+            takeFee = false;// 普通用户间转账
+        }
+
+        if (takeFee) {
+            bool isBuy = automatedMarketMakerPairs[from];
+            bool isSell = automatedMarketMakerPairs[to];
+            if (isBuy) {
+
+            } else if (isSell) {
+
+            }
+        }
     }
+
 
     // ├─ Uniswap 集成
     // │  ├─ Router 接口
